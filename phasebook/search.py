0f8ef8c1971860179af427ababa75fc84ab98e86
JovieNielBaltazar <jovienielbaltazar@gmail.com>
2022-09-22 09:23:02 -0700

    Case Sensitive Fix for Bonus Challenge

--- phasebook/search.py
+++ phasebook/search.py
@@ -13,6 +13,7 @@ def search():
 
 def search_users(**kwargs):
 
+    print("test")
     output_list = []
     if len(kwargs) < 1:
         print(USERS)
@@ -22,20 +23,18 @@ def search_users(**kwargs):
         #converts id input from string to int
         if "id" in kwargs:
             kwargs.update({"id": str(kwargs["id"])})
+            
+        for key, value in kwargs.items():
+            for ind_dict in USERS:
+                if type(kwargs[key]) == str:
+                    if kwargs[key] == ind_dict[key] or kwargs[key].lower() in ind_dict[key].lower():
+                        if ind_dict not in output_list:
+                            output_list.append(ind_dict) 
+                    
+                elif type(kwargs[key]) == int:
+                    if ind_dict[key] in range(kwargs[key]-1, kwargs[key]+2):
+                        if ind_dict not in output_list:
+                            output_list.append(ind_dict) 
 
-        for ind_dict in USERS:
-            if kwargs.items() <= ind_dict.items():
-                if ind_dict not in output_list:
-                    output_list.append(ind_dict)
-            else:
-                for key, value in kwargs.items():
-                    if type(value) == int:
-                        if ind_dict[key] in range(value-1, value+2):
-                            if ind_dict not in output_list:
-                                output_list.append(ind_dict)
-                    else: 
-                        if ind_dict[key] == value or value.lower() in ind_dict[key].lower():
-                            if ind_dict not in output_list:
-                                output_list.append(ind_dict)
 
-    return output_list 
+    return output_list
